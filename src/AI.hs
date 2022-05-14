d79d5afccd2af00efa4df0d3402aa501acbdab2b
Jacob(bad at programming) <66473431+VeryBadAtPython@users.noreply.github.com>
2022-05-14 22:17:37 +1000

    First Steps

--- src/AI.hs
+++ src/AI.hs
@@ -20,8 +20,10 @@ data AIFunc
 -- | The table of all AIs that your assignment provides. The AI named
 -- "default" in this table is the one your tutor will dedicate most of
 -- his or her attention to marking.
+
 ais :: [(String, AIFunc)]
-ais = [ ("default", NoLookahead (firstLegalMove COMP1100))
+ais = [("default", NoLookahead (firstCaptureMove COMP1100)),
+      ("provided", NoLookahead (firstLegalMove COMP1100))
       ]
 
 -- | A very simple AI, which passes whenever it can, and if not,
@@ -35,3 +37,67 @@ firstLegalMove :: Course -> GameState -> Move
 firstLegalMove course state = case applyMove course Pass state of
   Nothing -> head (legalMoves state)
   _ -> Pass
+
+
+-- | ==================================================== | --
+-- | ================ First capture move ================ | --
+-- | ==================================================== | --
+{-
+  - Picks the first of the list of capturing moves...
+    ...otherwise just does the first legal move
+  - Needs to check if the captures list is empty:
+  - Then choose the first of it as a maybe type:
+  - if nothing just call firstLegalMove
+-}
+
+firstCaptureMove :: Course -> GameState -> Move
+firstCaptureMove course state = case applyMove course Pass state of
+  Nothing -> case captureHead (captures state) of
+    Just move -> move
+    _         -> head (legalMoves state)
+  _ -> Pass
+  where
+    captureHead :: [Move] -> Maybe Move
+    captureHead moves = case moves of
+      x:_ -> Just x
+      _    -> Nothing
+
+-- | ==================================================== | --
+-- | ================ Greedy             ================ | --
+-- | ==================================================== | --
+{-
+  - Needs the list of legalMoves
+  - Should traverse this list and pick the one which takes the most pieces.
+  - Such a traverse would return the head of the list if none are capturing
+    moves.
+  - Perhaps traverse the list and pair each one with the countPieces of other
+    player each remaining state which can then be sorted through to get the one
+    with the least remaining pieces
+  - Need a function that applies each move to game state to then apply 
+    countPieces to.
+-}
+
+-- Shortcut to enter my course if needed
+myC :: Course
+myC = COMP1100
+
+-- Count the opponent's number of pieces state resulting from a move
+-- Inputs a Maybe state and the opposing player
+oppPieces :: Maybe GameState -> Player -> Maybe Int
+oppPieces state player = case state of
+  Just c -> case (countPieces c) of
+    (p1,p2) -> if (player == Player1) then Just p1 else Just p2
+    _       -> Nothing
+  _      -> Nothing
+
+
+
+
+  -- | ================ Turn Checker ================ | --
+-- | A function that decides what to do based pon who's turn it is:
+{-
+whosTurn :: GameState -> Player
+whosTurn (State turn _ _ _ _) = case turn of
+  Player 1 -> undefined
+  Player 2 -> undefined
+-}
